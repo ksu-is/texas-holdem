3fed1a4d00f5ea54bef2fdb4681c008384dd5c5d
Matthew Kalep Conway <69817356+beborenay@users.noreply.github.com>
2020-11-17 14:11:45 -0500

    Update TexasHoldem.py
    the next change returns a list giving me a random card and the 1 represents the cards suit and the 0 represents the cards value.

--- TexasHoldem.py
+++ TexasHoldem.py
@@ -5,6 +5,14 @@ def linebreak(sec=2):
 	print('\n' + '*'*60 + '\n' + '*'*60 + '\n')
 	time.sleep(sec)
 
-# Lists for card variables to generate any of the possible 52 cards
 suit_list = ['clubs', 'diamonds', 'hearts', 'spades']
 value_list = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+
+def rand_card():
+	rand_suit = random.choice(suit_list)
+	rand_value = random.choice(value_list)
+	return [str(rand_value), rand_suit]
+def get_card_value(card):
+	return card[0]
+def get_card_suit(card):
+	return card[1]
