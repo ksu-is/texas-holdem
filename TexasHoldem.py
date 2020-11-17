7c0f020e5c87e98ab3fc3695dae2b77a40ad4e15
Matthew Kalep Conway <69817356+beborenay@users.noreply.github.com>
2020-11-17 14:29:43 -0500

    Update TexasHoldem.py
    i defied the player making it so the player is told what hand they are dealt. and it will also prevent giving the player the same card twice or more. so it gives them 1 copy of a different card.

--- TexasHoldem.py
+++ TexasHoldem.py
@@ -12,7 +12,22 @@ def rand_card():
 	rand_suit = random.choice(suit_list)
 	rand_value = random.choice(value_list)
 	return [str(rand_value), rand_suit]
+
 def get_card_value(card):
 	return card[0]
+# Gets the suit of the card
 def get_card_suit(card):
 	return card[1]
+
+def deal_player():
+	print('\nYou are dealt the following five cards:')
+	player_hand = []
+	i = 0
+	while i < 5:
+		letters_list = ['a', 'b', 'c', 'd', 'e']
+		given_card = rand_card()
+		if given_card not in player_hand:
+			print(' %s)   %s of %s' %(letters_list[i], get_card_value(given_card), get_card_suit(given_card)))
+			player_hand.append(given_card)
+			i = i + 1
+	return player_hand
