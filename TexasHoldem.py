dc310da5e2649e46d5060cce7249618daaf68e3d
Matthew Kalep Conway <69817356+beborenay@users.noreply.github.com>
2020-11-17 14:06:04 -0500

    Update TexasHoldem.py
    This change was to generate all possible 52 cards. So the suits and 2 - 10 then Ace, king. queen and jack

--- TexasHoldem.py
+++ TexasHoldem.py
@@ -1,10 +1,10 @@
-players = 2
-game = None
-myPoker = 0
+import random
+import time
 
-def Pokername(Poker):
-    global mypoker
-    if poker == myPoker:
-        return "you"
-    else:
-        return "Player %d" % poker
+def linebreak(sec=2):
+	print('\n' + '*'*60 + '\n' + '*'*60 + '\n')
+	time.sleep(sec)
+
+# Lists for card variables to generate any of the possible 52 cards
+suit_list = ['clubs', 'diamonds', 'hearts', 'spades']
+value_list = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
